f6331bf8176ec0ee1185bcc53a16575ddec3988d
aravind <aravind.b1604@gmail.com>
2025-07-25 21:17:29 +0530

    removed sysyout from submit user

--- ums/src/main/java/com/techm/ums/controller/UserController.java
+++ ums/src/main/java/com/techm/ums/controller/UserController.java
@@ -51,7 +51,6 @@ public class UserController {
 	@ResponseBody
 	public String submitUser(int id, String name)
 	{
-		System.out.println("name"+name);
 		String result =userServiceObj.createUser(id, name);
 		return result;
 	}
